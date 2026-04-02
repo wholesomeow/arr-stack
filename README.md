80f6c7c34ffebb8c76bcf2c5d2be39f5d8d3cff2
wholesomeow <drcappuchino2@gmail.com>
2026-04-02 17:40:47 -0600

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,39 @@
 # Arr-stack
+
+This repo contains the docker-compose and various scripts that I use to deploy my local arr-stack.
+
+# Set up instructions
+
+Run the `setup.sh` script and it will create all the needed directories for the arr-stack to deploy.
+Make changes to the `docker-compose.yml` file to adjust the containers that will be deployed.
+
+# Features and Services
+
+TBD
+
+# "Roadmap"
+
+It's not *technically* a roadmap, but the term does fit well enough.
+I know most people are fairly happy with just getting the thing deployed and using it, but I like to tinker. So here is where I'll be documenting things I want to add to the stack that I think could improve the QoL of use here.
+
+## Workflow Improvements
+
+- [ ] Use [Kapitan](https://github.com/kapicorp/kapitan) to generate a docker compose file from multiple settings files
+- [ ] Split out each docker container into it's own file for better customization and maintanence
+- [ ] Add features/services description to README (potentially auto-generated?)
+- [ ] Auto-update containers when available (not using renovate)
+
+## Additional Services
+
+- [ ] Implement [bookshelf](https://github.com/pennydreadful/bookshelf/pkgs/container/bookshelf)
+- [ ] Implement [SuggestArr](https://github.com/giuseppe99barchetta/SuggestArr)
+- [ ] Implement [reiverr](https://github.com/aleksilassila/reiverr?tab=readme-ov-file)
+- [ ] Implement [seerr](https://github.com/seerr-team/seerr)
+
+## General Improvements
+
+- [ ] Implement some form of disk space monitoring/categorization
+- [ ] Implement some form of log aggregation
+- [ ] Implement some form of container performance monitoring/alerting
+- [ ] Validate hardlinks are working correctly
+- [ ] Find cleaning script for my usecase
---
+++ setup.sh
@@ -0,0 +1,86 @@
+#!/usr/bin/env bash
+
+# Log Helpers
+info()  { echo "Info: $*"; }
+warn()  { echo "Warn: $*"; }
+ok()    { echo "Ok: $*"; }
+error() { echo "Error: $*"; echo; exit 1; }
+
+# Pushd to data location on Windows
+pushd /mnt/e/
+
+ROOT_FOLDER="arr-stack"
+MEDIA_FOLDER="${ROOT_FOLDER}/media"
+TORRENTS_FOLDER="${ROOT_FOLDER}/torrents"
+CONFIG_FOLDER="$HOME/arr-stack/config"
+
+info "Starting arr-stack setup"
+
+# Create the folder structure
+mkdir -p $ROOT_FOLDER
+
+# for media
+mkdir -p $MEDIA_FOLDER
+mkdir -p $MEDIA_FOLDER/downloads
+mkdir -p $MEDIA_FOLDER/downloads/complete
+mkdir -p $MEDIA_FOLDER/downloads/incomplete
+
+mkdir -p $MEDIA_FOLDER/anime
+mkdir -p $MEDIA_FOLDER/movies
+mkdir -p $MEDIA_FOLDER/music
+mkdir -p $MEDIA_FOLDER/tv
+
+info "Finished creating media folders"
+
+# for torrents
+mkdir -p $TORRENTS_FOLDER
+mkdir -p $TORRENTS_FOLDER/anime
+mkdir -p $TORRENTS_FOLDER/movies
+mkdir -p $TORRENTS_FOLDER/music
+mkdir -p $TORRENTS_FOLDER/tv
+
+info "Finished creating torrents folders"
+
+# for configs
+mkdir -p $CONFIG_FOLDER
+mkdir -p $CONFIG_FOLDER/sonarr
+mkdir -p $CONFIG_FOLDER/radarr
+mkdir -p $CONFIG_FOLDER/prowlarr
+mkdir -p $CONFIG_FOLDER/lidarr
+mkdir -p $CONFIG_FOLDER/bazarr
+mkdir -p $CONFIG_FOLDER/flaresolverr
+mkdir -p $CONFIG_FOLDER/qbittorrent
+mkdir -p $CONFIG_FOLDER/jellyfin
+
+info "Finished creating config folders"
+
+# Set permissions
+chown -R 1000:1000 $ROOT_FOLDER
+chmod -R a=,a+rX,u+w,g+w $ROOT_FOLDER
+
+# Install docker/docker compose
+# Add Docker's official GPG key:
+sudo apt update
+sudo apt install ca-certificates curl
+sudo install -m 0755 -d /etc/apt/keyrings
+sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg -o /etc/apt/keyrings/docker.asc
+sudo chmod a+r /etc/apt/keyrings/docker.asc
+
+# Add the repository to Apt sources:
+sudo tee /etc/apt/sources.list.d/docker.sources <<EOF
+Types: deb
+URIs: https://download.docker.com/linux/ubuntu
+Suites: $(. /etc/os-release && echo "${UBUNTU_CODENAME:-$VERSION_CODENAME}")
+Components: stable
+Signed-By: /etc/apt/keyrings/docker.asc
+EOF
+
+sudo apt update
+sudo apt install docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
+
+ok "All done c:"
+info "Here's what was made:"
+tree $ROOT_FOLDER
+
+# Return to where we were
+popd
